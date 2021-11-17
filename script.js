e83f3942be1e23892877c5437352da1c2a233766
Jan Carlo Avendano <jancarloa0524@hstat.org>
2021-11-17 06:37:00 +0000

    added check() func that checks whether or no the bill is split, to change the output

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function tipCalc(tipPercentage, initialTotal, numOfPeople) {
-  initialTotal = prompt("What is the total? (Please only write the number, example: 50)")
-  tipPercentage = prompt("What is the tip percentage? (Example: 5%)")
-  numOfPeople = prompt("Split between how many people? (Please enter a number, example: 2)")
-  document.write("You need to pay $" + (Math.round(((((parseFloat(tipPercentage) / 100) * parseInt(initialTotal)) + parseInt(initialTotal)) / parseInt(numOfPeople)) * 100) / 100) + "!")
+alert("Welcome to my Tip Calculator!");
+var numOfPeople
+printAns();
+check();
+
+function tipCalc(tipPercentage, initialTotal, finalTotal) {
+  initialTotal = prompt("What is the total? (Please only write the number, example: 50)");
+  tipPercentage = prompt("What is the tip percentage? (Example: 5%)");
+  numOfPeople = prompt("Split between how many people? (Please enter a number, example: 2)");
+  finalTotal = (Math.round(((((parseFloat(tipPercentage) / 100) * parseInt(initialTotal)) + parseInt(initialTotal)) / parseInt(numOfPeople)) * 100) / 100)
+  return finalTotal;
 }
 
-tipCalc()
+function check() {
+  console.log(numOfPeople)
+  if (numOfPeople === "1") {
+    document.write("!")
+  } else {
+    document.write(" of the total!")
+  }
+}
+
+function printAns() {
+  document.write("You need to pay $" + tipCalc())
+}
